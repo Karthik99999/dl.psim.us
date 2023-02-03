8504bb79ca10f9142804b391099374b672a21467
Annika L <annika0uwu@gmail.com>
2023-02-02 17:55:38 -0800

    Add UI for team management

--- data/mods/draftarena/random-teams.ts
+++ data/mods/draftarena/random-teams.ts
@@ -1,8 +1,9 @@
 import type {PRNG, PRNGSeed} from '../../../sim/prng';
 
 import RandomTeams from '../../random-teams';
-import {teamPairs} from './team-data';
-import {Teams} from '../../../sim';
+import {FS, Teams} from '../../../sim';
+
+const TEAMS_FILE = 'config/draft-arena-teams.json';
 
 // Generates teams for Draft Arena, a format in which teams are chosen randomly from a pool of pairs.
 // Since teams are paired, if you know Player 1's team, you can determine Player 2's.
@@ -11,10 +12,16 @@ import {Teams} from '../../../sim';
 export class DraftArenaTeams extends RandomTeams {
 	// If we've already chosen a pair, the team not used yet goes in here.
 	leftoverTeam: PokemonSet[] | null;
+	teamPairs: [string, string][];
 
 	constructor(format: Format | string, prng: PRNG | PRNGSeed | null) {
 		super(format, prng);
 		this.leftoverTeam = null;
+		try {
+			this.teamPairs = JSON.parse(FS(TEAMS_FILE).readSync());
+		} catch (e: any) {
+			this.teamPairs = [];
+		}
 	}
 
 	checkedImportTeam(team: string): PokemonSet[] {
@@ -35,8 +42,12 @@ export class DraftArenaTeams extends RandomTeams {
 	randomPairedTeam(): PokemonSet[] {
 		if (this.leftoverTeam) return this.leftoverTeam;
 
+		if (!this.teamPairs.length) {
+			throw new Error(`There are no teams in the Draft Arena pool. Tell your local system administrator to add teams with /draftarena teams`);
+		}
+
 		// pick a random team pair to use
-		const pair = this.sample(teamPairs);
+		const pair = this.sample(this.teamPairs);
 
 		// typescript should check this for us, but just in case we'll assert our assumptions here.
 		if (pair.length !== 2) throw new Error(`Team pair in ${this.format.name} is not of length 2.`);
